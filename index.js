3a721b900889c20cbcf20d389bee287fab0ac7f8
Morgan <morganlbyrne@gmail.com>
2021-11-04 18:09:26 -0400

    commit tree adf0e1b5207bf8e0749674bff25382fd0898ac1d

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching(listOfDrivers, name) {
+    return listOfDrivers.filter((drivers) => (drivers.toLowerCase() === name.toLowerCase()))
+}
+
+function fuzzyMatch(listOfDrivers,letters) {
+    
+    return listOfDrivers.filter((drivers) => drivers.toLowerCase().indexOf(letters.toLowerCase()) === 0)
+}
+
+
+function matchName(listOfDrivers, string) {
+
+    return listOfDrivers.filter( (listOfDrivers) => listOfDrivers.name === string)
+}
+
+
+console.log(matchName(drivers, 'Bobby'))
